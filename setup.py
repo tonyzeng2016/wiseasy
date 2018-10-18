c791339f45ed20a2e5b5044e151d692618c910db
zengxiaolong <zengxiaolong@chinafintech.cn>
2018-10-18 14:04:53 +0800

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 #encoding=utf-8
 from setuptools import setup
-
 version = "0.0.1"
-
-
 setup(
     name="wiseasy",
     version=version,
     url='https://github.com/tonyzeng2016/wiseasy',
-    project_urls={
-        "Documentation": "https://wiseasy.readthedocs.io/",
-    },
     description='the utility in wiseasy',
     long_description=u'''
     2018工作日判断;
@@ -19,6 +13,7 @@ setup(
     install_requires=[
         "pandas","numpy"
     ],
+    packages=['wiseasy'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 2',
