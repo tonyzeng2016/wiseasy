7617d2e74af6d51c25599fc9a6b72585e7b46007
zengxiaolong <zengxiaolong@chinafintech.cn>
2018-10-22 11:10:02 +0800

    update

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "pandas","numpy"
     ],
     packages=['wiseasy'],
+    include_package_data=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 2',
