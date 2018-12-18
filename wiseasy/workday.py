dfe9fc1ba112e8158a1c668388f084ef3d8768d7
zengxiaolong <zengxiaolong@chinafintech.cn>
2018-12-18 10:18:50 +0800

    添加2019年放假安排

--- wiseasy/workday.py
+++ wiseasy/workday.py
@@ -27,7 +27,7 @@ def __wiseasy_workday_diff__(date,diff=1):
     
     for i in range(1,np.abs(diff)+30):
         currentDte=dateTime+datetime.timedelta(days=i*flag)
-        isworkday=__wiseasy_workday__(currentDte) if currentDte.year==2018 else False
+        isworkday=__wiseasy_workday__(currentDte) if currentDte.year in [2018,2019] else False
         if isworkday:counter+=1
         #print i,currentDte,isworkday,counter
         if counter>=np.abs(diff):return currentDte
@@ -43,20 +43,29 @@ def __wiseasy_workday__(date):
     else:
         raise NotImplementedError()
     
-    offdays={u'元旦':{u'begin':u'2017-12-30',u'end':u'2018-01-01'},
-            u'春节':{u'begin':u'2018-02-15',u'end':u'2018-02-21'},
-            u'清明节':{u'begin':u'2018-04-05',u'end':u'2018-04-07'},
-            u'劳动节':{u'begin':u'2018-04-29',u'end':u'2018-05-01'},
-            u'端午节':{u'begin':u'2018-06-16',u'end':u'2018-06-18'},
-            u'中秋节':{u'begin':u'2018-09-22',u'end':u'2018-09-24'},
-            u'国庆节':{u'begin':u'2018-10-01',u'end':u'2018-10-07'}
+    offdays={u'2018元旦':{u'begin':u'2017-12-30',u'end':u'2018-01-01'},
+            u'2018春节':{u'begin':u'2018-02-15',u'end':u'2018-02-21'},
+            u'2018清明节':{u'begin':u'2018-04-05',u'end':u'2018-04-07'},
+            u'2018劳动节':{u'begin':u'2018-04-29',u'end':u'2018-05-01'},
+            u'2018端午节':{u'begin':u'2018-06-16',u'end':u'2018-06-18'},
+            u'2018中秋节':{u'begin':u'2018-09-22',u'end':u'2018-09-24'},
+            u'2018国庆节':{u'begin':u'2018-10-01',u'end':u'2018-10-07'},
+            
+            u'2019元旦':{u'begin':u'2018-12-30',u'end':u'2019-01-01'},
+            u'2019春节':{u'begin':u'2019-02-04',u'end':u'2019-02-10'},
+            u'2019清明节':{u'begin':u'2019-04-05',u'end':u'2019-04-05'},
+            u'2019劳动节':{u'begin':u'2019-05-01',u'end':u'2019-05-01'},
+            u'2019端午节':{u'begin':u'2019-06-07',u'end':u'2019-06-07'},
+            u'2019中秋节':{u'begin':u'2019-09-13',u'end':u'2019-09-13'},
+            u'2019国庆节':{u'begin':u'2019-10-01',u'end':u'2019-10-07'},
             }
-    ondays={u'春节':[u'2018-02-11',u'2018-02-24'],
+    ondays={u'元旦':[u'2018-12-29'],
+            u'春节':[u'2018-02-11',u'2018-02-24',u'2019-02-02',u'2019-02-03'],
             u'清明节':[u'2018-04-08'],
             u'劳动节':[u'2018-04-28'],
-            u'国庆节':[u'2018-09-29',u'2018-09-30']
+            u'国庆节':[u'2018-09-29',u'2018-09-30',u'2019-09-29',u'2019-10-12']
             }
-    if dateTime.year!=2018:return False
+    if dateTime.year not in [2018,2019]:return False
     (_,_,day)=dateTime.isocalendar()
     
     for _,v in offdays.iteritems():
